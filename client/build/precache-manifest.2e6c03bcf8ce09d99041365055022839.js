7411c310baa3ff5e5deacfe0d4bb5830616b98be
langel <luiz.angel@hummingbird.com.br>
2019-04-14 12:55:26 -0300

    fixing deploy

---
+++ client/build/precache-manifest.2e6c03bcf8ce09d99041365055022839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "296ac7c35487c4e22942",
+    "url": "/static/css/main.95d90433.chunk.css"
+  },
+  {
+    "revision": "296ac7c35487c4e22942",
+    "url": "/static/js/main.44cfe5c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f5a2ba41477b987e791",
+    "url": "/static/js/2.5fd39cd3.chunk.js"
+  },
+  {
+    "revision": "f123b79be75ebc227fb75b7d851abb57",
+    "url": "/static/media/heartbeat.f123b79b.svg"
+  },
+  {
+    "revision": "ad42b580318346141df9c81e31e11f28",
+    "url": "/index.html"
+  }
+];
